c8bc3d656ce96b39b2061cc0d69f1e2df49d2342
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2023-11-07 17:05:54 -0600

    list-users

--- lib/modules/auth/adapters/screens/profile.dart
+++ lib/modules/auth/adapters/screens/profile.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:learning_app_10a/kernel/theme/colors_app.dart';
+import 'package:learning_app_10a/modules/auth/adapters/screens/widgets/list_user.dart';
 
 class Profile extends StatelessWidget{
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Perfil')),
+    final Map item={"name":"Thayli Villegas"};
+    final Map item2={"name":"Thayli Villegas"};
+    final List items=[item,item2];
+    return Scaffold(
+      appBar: AppBar( title: const Text('Usuarios'),backgroundColor: ColorsApp.primaryColor,foregroundColor: Colors.white,),
+      body: Padding(
+        padding:const  EdgeInsets.all(16.0),
+        child: ListView.builder(itemCount:items.length, itemBuilder:(context,index){
+          return ListUser(item: items[index],);
+
+        }),
+      )
+      
     );
   }
 
